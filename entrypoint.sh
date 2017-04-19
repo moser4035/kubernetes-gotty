38e6d89c0093e4ffa554c07a17e4442d1e4cea72
dmoser <david.moser@bitmovin.com>
2017-04-19 10:41:33 +0200

    Added initial command 'kc top pods' to entrypoint.sh Changed version to 0.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ if [[ -n $BASIC_AUTH_USER || -n $BASIC_AUTH_PASS ]]; then
 	PARAMS="$PARAMS --credential $BASIC_AUTH_USER:$BASIC_AUTH_PASS"
 fi
 
-gotty $PARAMS /bin/bash
+gotty $PARAMS '/bin/bash kc top pods'
 
